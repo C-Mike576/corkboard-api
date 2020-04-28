05571e562a87aa8277d91429bed6ab30e9e800d3
C-Mike576 <m.calkins576@gmail.com>
2020-04-28 15:19:05 -0400

    added server actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,31 @@
 class PostsController < ApplicationController
 
     def index
-        post = Post.all
-        render json:post
+        posts = Post.all
+        render json:posts
     end
 
     def create
         newPost = Post.create(username: params['username'], content: params['content'])
         render json:newPost
     end
+
+    def show
+        post = Post.find(params[:id])
+        render json:post
+    end
+
+    def update
+        post = Post.find(params[:id])
+        post.content = params[:content]
+        post.save
+        render json:post
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.delete
+        render "post deleted"
+    end
+
 end
